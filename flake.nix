d1ebf581c8d4fa67e0ad8933a46393a111968ea0
VicentePSalcedo <vicentepsalcedo@gmail.com>
2024-04-07 15:54:20 -0400

    Sun Apr  7 03:54:20 PM EDT 2024

--- flake.nix
+++ flake.nix
@@ -2,10 +2,14 @@
   description = "Sintra's flake templates";
 
   outputs = { self , ... }: {
-      templates = {
-        angular-firebase-static = {
-          path = ./angular-firebase-static;
-          description = "Latest Angular and firebse setup for a static site.";
+    templates = {
+      angular-firebase-static = {
+        path = ./angular-firebase-static;
+        description = "Latest Angular and firebse setup for a static site.";
+      };
+      python = {
+        path = ./python;
+        description = "Console python.";
       };
     };
   };
---
+++ python/flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      # create an environment with nodejs_18, pnpm, and yarn
+      packages = with pkgs; [
+        python3
+        python311Packages.pip
+        virtualenv
+      ];
+
+      shellHook = ''
+      '';
+    };
+    defaultTemplate.path = ./.;
+  };
+}
